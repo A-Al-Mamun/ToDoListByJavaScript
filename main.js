e2ba4d64dea669b86efeee65928f295173f65ebf
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-12 00:07:56 +0600

    To do list Menu Bar setup by js

--- main.js
+++ main.js
@@ -5,10 +5,20 @@ bar.addEventListener('click', () => {
     let menuFont = document.getElementsByClassName('menu-font')[0];
     let search = document.getElementsByClassName('search')[0];
 
-    let tasks = document.querySelectorAll('.task');
+    let task0 = document.getElementsByClassName('task')[0];
+    let task1 = document.getElementsByClassName('task')[1];
 
-    leftMenu.style.display = 'flex-basis(10%)';
-    menuFont.style.display = 'none';
-    search.style.display = 'none';
-    tasks.style.display = 'none';
+    if(task0.style.display == 'none'){
+        leftMenu.style.display = 'flexBasis: 10%';
+        menuFont.style.display = 'block';
+        search.style.display = 'block';
+        task0.style.display = 'flex';
+        task1.style.display = 'flex';
+    }else{
+        leftMenu.style.width = '10%'
+        menuFont.style.display = 'none';
+        search.style.display = 'none';
+        task0.style.display = 'none';
+        task1.style.display = 'none';
+    }
 })
